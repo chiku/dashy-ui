9c51a9f34356150c412cc8ae40d9f08ef25c017c
Chirantan Mitra <chirantan.mitra@gmail.com>
2017-01-31 23:37:27 +0530

    Migrate to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,31 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/main.js',
+  entry: './src/main.js',
 
-    output: {
-        path: __dirname,
-        filename: 'out/public/app.js',
-        publicPath: '/public/'
-    },
+  output: {
+    path: __dirname,
+    filename: 'out/public/app.js',
+    publicPath: '/public/'
+  },
 
-    stats: {
-        colors: true,
-        reasons: true
-    },
+  stats: {
+    colors: true,
+    reasons: true
+  },
 
-    devtool: 'source-map',
+  devtool: 'source-map',
 
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /(bower_components)/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015']
-            }
-        }]
-    },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+    }]
+  },
 
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.DedupePlugin()
-    ]
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+    }),
+  ]
 };
