d565d1cd9ae6d94e2e601029d0cb157a1e0f4d4c
Chirantan Mitra <chirantan.mitra@gmail.com>
2017-02-10 21:48:21 +0530

    Use webpack to manage CSS & HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: [
+    './src/main.js',
+    './src/main.css',
+  ],
 
   output: {
-    path: __dirname,
-    filename: 'out/public/app.js',
-    publicPath: '/public/',
+    path: './out/public',
+    filename: 'app.js',
+    publicPath: '/',
   },
 
   stats: {
@@ -17,15 +22,30 @@ module.exports = {
   devtool: 'source-map',
 
   module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-    }],
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader?minimize',
+        }),
+      },
+    ],
   },
 
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
+      output: { comments: false },
+    }),
+    new ExtractTextPlugin('main.css'),
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs',
+      minify: { collapseWhitespace: true },
     }),
   ],
 };
